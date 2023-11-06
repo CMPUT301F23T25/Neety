fe11c60e57f2e797112f2a1e72096fa0a3c22b0e
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2023-11-05 18:40:38 -0700

    feat: 🎸 items
    add items class, constatnts, git cleanup

--- app/src/main/java/com/team25/neety/Constants.java
+++ app/src/main/java/com/team25/neety/Constants.java
@@ -1,2 +1,8 @@
-package com.team25.neety;public class Constants {
+package com.team25.neety;
+
+public final class Constants {
+    public static final int NO_SERIAL = -1;
+
+    // Private constructor because you should never instantiate this class
+    private Constants() {}
 }
