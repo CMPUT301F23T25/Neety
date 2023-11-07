78b1f2d6e46c6c27c96659d3a21a79f33e976915
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2023-11-07 02:04:15 -0700

    feat: 🎸 view
    completely refactor app navi, add items view, add extra helper getters
    to item class

--- app/src/main/java/com/team25/neety/Constants.java
+++ app/src/main/java/com/team25/neety/Constants.java
@@ -3,6 +3,10 @@ package com.team25.neety;
 public final class Constants {
     public static final int NO_SERIAL = -1;
 
+    public static final String DATE_FORMAT_PATTERN = "MM/dd/yyyy HH:mm:ss";
+
+    public static final String INTENT_ITEM_KEY = "PASSED_ITEM";
+
     // Private constructor because you should never instantiate this class
     private Constants() {}
 }
