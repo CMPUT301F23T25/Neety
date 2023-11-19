47d9a720b63debb87490633f196442c1d9b2f44d
Bao Ng <pbnguyen@ualberta.ca>
2023-11-19 13:58:27 -0700

    add: deleting images
    simple deleting method: clicking on the delete button on each image to delete one by one.
    May add/change to other methods on request

--- app/src/main/java/com/team25/neety/ImageAdapter.java
+++ app/src/main/java/com/team25/neety/ImageAdapter.java
@@ -1,16 +1,20 @@
 package com.team25.neety;
 
-import android.app.Activity;
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -22,6 +26,7 @@ import java.util.Set;
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
     private List<String> imageUrls;
     private Context context;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
 
     public ImageAdapter(Context context, List<String> imageUrls) {
         this.context = context;
@@ -32,9 +37,9 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ImageView imageView = (ImageView) LayoutInflater.from(parent.getContext())
+        View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_image, parent, false);
-        return new ImageViewHolder(imageView);
+        return new ImageViewHolder(itemView);
     }
 
     @Override
@@ -58,7 +63,32 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
                         Log.e("ImageAdapter", "Error loading image", e);
                     }
                 });
+
+        holder.imageDelButton.setOnClickListener(v -> {
+            int pos = holder.getAdapterPosition();
+            if (pos != RecyclerView.NO_POSITION) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Delete Image")
+                        .setMessage("Are you sure you want to delete this image?")
+                        .setPositiveButton("Yes", (dialog, which) -> {
+                            // Continue with delete operation
+                            StorageReference imageRef = storage.getReferenceFromUrl(imageUrl);
+                            imageRef.delete().addOnSuccessListener(aVoid -> {
+                                // Image deleted successfully
+                                imageUrls.remove(position);
+                                notifyItemRemoved(position);
+                            }).addOnFailureListener(e -> {
+                                // Error occurred while deleting image
+                                Log.e("ImageAdapter", "Error deleting image", e);
+                            });
+                        })
+                        .setNegativeButton("No", null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
@@ -67,9 +97,12 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
 
     static class ImageViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
-        ImageViewHolder(@NonNull ImageView imageView) {
-            super(imageView);
-            this.imageView = imageView;
+        ImageButton imageDelButton;
+
+        ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView = itemView.findViewById(R.id.imageView);
+            this.imageDelButton = itemView.findViewById(R.id.image_del_button);
         }
     }
 }
