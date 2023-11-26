173369233d2c10135ad3f383b181d3708e73e44c
Bao Nguyen <pbnguyen@ualberta.ca>
2023-11-25 20:40:10 -0700

    [UI] profile button

--- app/src/main/java/com/team25/neety/MainActivity.java
+++ app/src/main/java/com/team25/neety/MainActivity.java
@@ -3,6 +3,9 @@ package com.team25.neety;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import android.util.Log;
@@ -282,5 +285,20 @@ public class MainActivity extends AppCompatActivity implements AddItem.OnFragmen
                 });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_button) {
+            // Handle action button click
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
