10a13c79b7534411f054420f499b5524645c375a
Swayam0301 <115033836+Swayam0301@users.noreply.github.com>
2023-11-29 22:07:09 -0700

    Some changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
             )
         }
     }
@@ -36,19 +36,21 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-auth:19.4.0")
-    implementation("com.google.android.gms:play-services-auth:18.1.0")
-    implementation("com.github.bumptech.glide:glide:4.11.0")
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment:2.7.2")
-    implementation("androidx.navigation:navigation-ui:2.7.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation (platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation ("com.google.firebase:firebase-database")
+    implementation ("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth")
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation ("com.github.bumptech.glide:glide:4.11.0")
+    implementation ("com.google.firebase:firebase-firestore")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.10.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.navigation:navigation-fragment:2.7.2")
+    implementation ("androidx.navigation:navigation-ui:2.7.2")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/team25/neety/LoginActivity.java
+++ app/src/main/java/com/team25/neety/LoginActivity.java
@@ -3,111 +3,139 @@ package com.team25.neety;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.common.SignInButton;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.FirebaseApp;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    // Initialize variables
     SignInButton btSignIn;
+    Button continueBtn, registerBtn;
+    TextView usernameAvailabilityTextView;
+
     GoogleSignInClient googleSignInClient;
     FirebaseAuth firebaseAuth;
+    DatabaseReference usersReference;
+    EditText usernameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FirebaseApp.initializeApp(this);
 
-        // Assign variable
         btSignIn = findViewById(R.id.bt_sign_in);
+        continueBtn = findViewById(R.id.continue_btn);
+        registerBtn = findViewById(R.id.register_btn);
+        usernameAvailabilityTextView = findViewById(R.id.username_availability_text);
 
-        // Initialize sign in options the client-id is copied form google-services.json file
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("708622187336-h5500e4k64ie4knkj3voeprh452m3mpa.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
 
-        // Initialize sign in client
         googleSignInClient = GoogleSignIn.getClient(LoginActivity.this, googleSignInOptions);
 
-        btSignIn.setOnClickListener((View.OnClickListener) view -> {
-            // Initialize sign in intent
+        firebaseAuth = FirebaseAuth.getInstance();
+        usersReference = FirebaseDatabase.getInstance().getReference("users");
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            checkUsernameAvailabilityAndProceed(firebaseUser.getUid());
+        }
+
+        btSignIn.setOnClickListener(view -> {
             Intent intent = googleSignInClient.getSignInIntent();
-            // Start activity for result
             startActivityForResult(intent, 100);
         });
 
-        // Initialize firebase auth
-        firebaseAuth = FirebaseAuth.getInstance();
-        // Initialize firebase user
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        // Check condition
-        if (firebaseUser != null) {
-            // When user already sign in redirect to profile activity
-            startActivity(new Intent(LoginActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        }
+        continueBtn.setOnClickListener(view -> {
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+            if (currentUser != null) {
+                checkUsernameAvailabilityAndProceed(currentUser.getUid());
+            }
+        });
+
+        registerBtn.setOnClickListener(view -> {
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+            if (currentUser != null) {
+                String enteredUsername = getUsernameFromEditText();
+                saveUsernameToDatabase(currentUser.getUid(), enteredUsername);
+                checkUsernameAvailabilityAndProceed(currentUser.getUid());
+            }
+        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Check condition
-        if (requestCode == 100) {
-            // When request code is equal to 100 initialize task
-            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-            // check condition
-            if (signInAccountTask.isSuccessful()) {
-                // When google sign in successful initialize string
-                String s = "Google sign in successful";
-                // Display Toast
-                displayToast(s);
-                // Initialize sign in account
-                try {
-                    // Initialize sign in account
-                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
-                    // Check condition
-                    if (googleSignInAccount != null) {
-                        // When sign in account is not equal to null initialize auth credential
-                        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
-                        // Check credential
-                        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                // Check condition
-                                if (task.isSuccessful()) {
-                                    // When task is successful redirect to profile activity display Toast
-                                    startActivity(new Intent(LoginActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                                    displayToast("Firebase authentication successful");
-                                } else {
-                                    // When task is unsuccessful display Toast
-                                    displayToast("Authentication Failed :" + task.getException().getMessage());
-                                }
-                            }
-                        });
+    private void checkUsernameAvailabilityAndProceed(String uid) {
+        String enteredUsername = getUsernameFromEditText();
+
+        usersReference.child(uid).child("username").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String storedUsername = dataSnapshot.getValue(String.class);
+                    if (enteredUsername.equals(storedUsername)) {
+                        // Username exists, proceed to main activity
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                        finish(); // Finish the LoginActivity
+                    } else {
+                        // Username does not match, show text view and make register button visible
+                        usernameAvailabilityTextView.setVisibility(View.VISIBLE);
+                        registerBtn.setVisibility(View.VISIBLE);
                     }
-                } catch (ApiException e) {
-                    e.printStackTrace();
+                } else {
+                    // Username does not exist, show text view and make register button visible
+                    usernameAvailabilityTextView.setVisibility(View.VISIBLE);
+                    registerBtn.setVisibility(View.VISIBLE);
                 }
             }
-        }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+            }
+        });
+    }
+
+    private void saveUsernameToDatabase(String uid, String username) {
+        usersReference.child(uid).child("username").setValue(username).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // Username saved successfully, proceed to main activity
+                startActivity(new Intent(LoginActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                finish(); // Finish the LoginActivity
+            } else {
+                // Handle error
+                displayToast("Error saving username: " + task.getException().getMessage());
+            }
+        });
+    }
+
+    private String getUsernameFromEditText() {
+        usernameEditText = findViewById(R.id.username);
+        return usernameEditText.getText().toString();
     }
 
     private void displayToast(String s) {
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
     }
 }
-
